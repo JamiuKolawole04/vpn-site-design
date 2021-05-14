f08f6e2fab99a720500b273233cacb580cf6b2a6
JamiuKolawole04 <75920331+JamiuKolawole04@users.noreply.github.com>
2021-05-15 00:37:37 +0100

    Add files via upload

---
+++ Lasles VPN Site/mobileNav.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = '100%' ;
+    document.getElementById("mySidenav").style.backgroundColor = "white"
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = '0' ;
+}  
